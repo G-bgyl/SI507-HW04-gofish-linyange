2b949454e09a23a56465d6f019e86e833c73f184
G-bgyl <gelinyan0801@foxmail.com>
2018-02-02 16:02:38 -0500

    fix the empty hand problem and return problem

--- go_fish.py
+++ go_fish.py
@@ -1,8 +1,8 @@
 import random
 
-deck = ['ace', 'ace', 'ace', 'ace', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5', '6',
+deck = [ '4', '4', '4', '4', '5', '5', '5', '5', '6',
         '6', '6', '6', '7', '7', '7', '7']
-''', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', 'jack',
+''''ace', 'ace', 'ace', 'ace', '2', '2', '2', '2', '3', '3', '3', '3',     , '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', 'jack',
         'jack', 'jack', 'jack', 'queen', 'queen', 'queen', 'queen', 'king', 'king', 'king', 'king' '''
 my_hand = []
 opponent_hand = []
@@ -13,42 +13,45 @@ pointer={0:['Alice',my_hand,my_score],1:['Bob',opponent_hand,opponent_score]}
 my_score = 0
 opponent_score = 0
 
-def draw(cards, owner):
+def draw(cards, owner,name,exit = False):
     if len(deck) >= cards:
         for i in range(0, cards):
             drawn_card = deck.pop((int)(random.random() * len(deck)))
             owner.append(drawn_card)
-            if owner == my_hand:
+            if name=='Alice':
                 print("Alice drew a %r." % drawn_card)
             else:
                 print("Bob drew a %r." % drawn_card)
         if cards ==1:
-            return drawn_card
+            return (drawn_card,exit)
     else:
-        print("No more cards in the deck.")
+        print("No more cards in the deck.\n")
         if not my_hand and not opponent_hand:
             exit = True
-    if owner == my_hand:
-        print("Alicia's hand:", my_hand)
-    elif owner ==opponent_hand:
-        print("Bob's hand:", opponent_hand)
-
+    if name=='Alice':
+        print("Alicia's hand:", my_hand,'\n')
+    elif name=='Bob':
+        print("Bob's hand:", opponent_hand,'\n')
+    drawn_card=None
+    print(drawn_card, exit)
+    re = (drawn_card, exit)
+    return re
 def check_hands(hand):
     i=0
     for each in hand[1]:  # checks hand for duplicates
         num = hand[1].count(each)
         if num == 4:
             i+=1
-            print(hand[0], " have four of %rs! yay!" % each)
+            print(hand[0], " have four of %rs! yay!" % each,'\n')
 
             for i in range(4):
                 hand[1].remove(each)
             hand[2] += 1
-            print(hand)
-            print(hand[0], "'s current hand:", hand[1])
+            # print(hand)
+            # print(hand[0], "'s current hand:", hand[1],'\n')
             return hand[2]
     if i ==0:
-        print('better luck next time!')
+        # print(hand[0], 'didn\'t have same four cards for now, better luck next time!','\n')
         return hand[2]
 
 def search_fish(giver,receiver,t):
@@ -66,7 +69,7 @@ def search_fish(giver,receiver,t):
 
         if i == 0 :
             print('Go fish!')
-            drawn_card=draw(1,receiver[1])
+            drawn_card=draw(1,receiver[1],receiver[0])[0]
             if drawn_card != t:
                 return False
             elif  drawn_card == t:
@@ -75,22 +78,22 @@ def search_fish(giver,receiver,t):
             return True
 
 
-def print_result(my_score,opponent_score):
-    print("Your score:", my_score)
-    print("Your opponent's score:", opponent_score)
-    if my_score > opponent_score:
-        print("Congratulations! You won!")
+def print_result(this_hand,other_hand):
+    print(this_hand[0],"'s score:", this_hand[2])
+    print(other_hand[0], "'s score:", other_hand[2])
+    if this_hand[2] > other_hand[2]:
+        print("Congratulations! {} won!".format(this_hand[0]))
 
-    elif opponent_score > my_score:
-        print("You suck! Your opponent won!")
+    elif this_hand[2] < other_hand[2]:
+        print("Congratulations! {} won!".format(other_hand[0]))
 
-    elif opponent_score == my_score:
+    elif this_hand[2] == other_hand[2]:
         print("You tied! Bummer!")
 
 def start_game():
     print('Let\'s begin!')
-    draw(7, my_hand)
-    draw(7, opponent_hand)
+    draw(7, my_hand,'Alice')
+    draw(7, opponent_hand,'Bob')
 
     exit = False
 
@@ -99,20 +102,21 @@ def start_game():
 
         this_hand = pointer[(round % 2)]
         other_hand =pointer[1-(round % 2)]
+        this_hand[2] = check_hands(this_hand)
+        other_hand[2] = check_hands(other_hand)
 
         iterate = True
         while iterate:
-            this_hand[2] = check_hands(this_hand)
-            other_hand[2] = check_hands(other_hand)
+
             t = input("%r, please choose a card from your hand to ask the opponent, or 'exit' to exit:\n" % this_hand[0])
             t = t.lower()
             while t == 'exit' or t not in this_hand[1]:
                 if t == "exit":
                     exit = True
-                    print("You will now exit the game.")
+                    print("You will now exit the game.",'\n')
                     break
                 elif t not in this_hand[1] :
-                    print("Sorry, you didn't choose a card from your hand.")
+                    print("Sorry, you didn't choose a card from your hand.",'\n')
                     t = input( "%r ,please choose a card from your hand to ask the opponent, or 'exit' to exit:\n" % this_hand[0])
 
             if t == "exit":
@@ -124,12 +128,21 @@ def start_game():
                 this_hand[2] = check_hands(this_hand)
 
             if len(this_hand[1])==0:
-                draw(1,this_hand)
+                result = (0,'k')
+                result = draw(1,this_hand[1],this_hand[0])
+                exit=result[1]
             if len(other_hand[1])==0:
-                draw(1,other_hand)
-            print('\n\nAlice:',my_hand,'\nBob:',opponent_hand,'\n\n')
+                result_1 = (0, 'k')
+                result_1 = draw(1,other_hand[1],other_hand[0])
+                exit = result_1[1]
+            if len(this_hand[1])!=0 and len(other_hand[1])!=0:
+                print('\n\nAlice:',my_hand,'\nBob:',opponent_hand,'\n\n')
+
+            if exit==True:
+                iterate=False
+                break
         round +=1
-    print_result(my_score, opponent_score)
+    print_result(this_hand, other_hand)
 
 
 
